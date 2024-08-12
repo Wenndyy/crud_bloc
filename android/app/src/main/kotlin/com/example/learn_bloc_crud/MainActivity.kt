75576f9761113f4e4516bd4d8f5dd915993f6bed
Wenndyy <dywenaja@gmail.com>
2024-08-13 01:03:35 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/learn_bloc_crud/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learn_bloc_crud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ packages/user_repository/lib/src/user_repo.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../user_repository.dart';
+
+abstract class UserRepository {
+  Stream<User?> get user;
+
+  Future<void> signIn(String email, String password);
+
+  Future<void> logOut();
+
+  Future<MyUser> signUp(MyUser myUser, String password);
+
+  Future<void> resetPassword(String email);
+
+  //setUserData
+  Future<void> setUserData(MyUser user);
+
+  //getMyUser
+  Future<MyUser> getMyUser(String myUserId);
+}
